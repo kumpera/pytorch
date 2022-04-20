c1630ec07a6e1dd19d192c3fe58e90aa86a03469
Rodrigo Kumpera <kumpera@fb.com>
2022-04-20 17:30:42 +0000

    Introduce distributed checkpoint with ShardedTensor.
    Co-authored-by: Wen Zhang <zhangwen@fb.com>
    Co-authored-by: Yifu Wang <yifu@fb.com>

---
+++ torch/distributed/_checkpoint/state_dict_loader.py
@@ -0,0 +1,234 @@
+
+import io
+from typing import Any, Dict, List, Optional, Tuple
+
+import torch
+from torch.distributed._shard.sharded_tensor import (
+    ShardedTensor,
+    ShardedTensorMetadata
+)
+from torch.distributed._shard.sharding_spec._internals import (
+    validate_non_overlapping_shards_metadata,
+    _check_shard_metadata_pair_overlap,
+)
+
+from .metadata import (
+    BytesReadRequest,
+    TensorReadRequest,
+    Metadata,
+    ExtendedTensorMetadata,
+)
+from .resharding import (
+    prepare_sharded_tensor_read,
+    _shards_get_overlap_region_wrt_saved_tensor
+)
+from .storage_reader import StorageReader
+
+
+def _reshard_and_prepare_read_request(
+    state_dict: Dict[str, Any], metadata_from_storage: Metadata
+) -> Tuple[List[BytesReadRequest], List[TensorReadRequest]]:
+    """
+    Use the loaded metadata and the current state dict to map the saved tensors to current tensors
+
+    NOTE:
+    During the save,
+    """
+    tensor_read_requests = []
+    bytes_read_requests = []
+    for fqn, obj in state_dict.items():
+        if isinstance(obj, torch.Tensor):
+            tensor = obj.detach()
+            storage_size = tensor.nelement() * tensor.element_size()
+
+            rr = TensorReadRequest(
+                tensor=tensor,
+                storage_key=fqn,
+                offsets=tuple([0] * len(tensor.size())),
+                lengths=tensor.size(),
+            )
+
+            tensor_read_requests.append(rr)
+        elif isinstance(obj, ShardedTensor):
+            md = metadata_from_storage.state_dict_metadata[fqn]
+            tensor_read_requests += prepare_sharded_tensor_read(md, obj)
+        else:
+            # This is actually hard to handle correctly
+            # If the value is not a tensor but any random obj,
+            # we cannot just write whatever memory it points to inplace
+            # the best we can to is to replace it with an object of the same type
+            bytes_io = io.BytesIO()
+            brr = BytesReadRequest(
+                bytes=bytes_io,
+                storage_key=fqn,
+            )
+            bytes_read_requests.append(brr)
+
+    return (bytes_read_requests, tensor_read_requests)
+
+
+def load_state_dict(
+    state_dict: Dict[str, Any],
+    storage_reader: StorageReader,
+) -> None:
+    """
+    This public function defines the default behavior to load a state_dict
+
+    Sample Code
+    ```
+        my_model = MyModule()
+        optimizer = Adagrad(my_model.parameters())
+        ...
+
+        model_state_dict = my_model.state_dict()
+        optim_state_dict = optimizer.state_dict()
+        ...
+
+        # torch.distributed does not assume the the correctness of the state_dict
+        # the caller needs to ensure the correctness of the state_dict
+        optim_state_dict = some_function_to_cleanup_optim_state_dict(optim_state_dict)
+        ...
+
+        fs_storage_loader = torch.distributed._checkpoint.FileSystemLoader("/checkpoint/1")
+        torch.distributed._checkpoint.load_state_dict(
+            state_dict=model_state_dict,
+            storage_reader=fs_stroage_loader,
+        )
+        torch.distributed._checkpoint.load_state_dict(
+            state_dict=optim_state_dict,
+            storage_reader=fs_stroage_loader,
+        )
+
+        # module.load_state_dict() functon might have customized steps
+        # to flush the state_dict, must call them to
+        # ensure the correct behavior
+        my_model.load_state_dict(model_state_dict)
+        optim_state_dict.load_state_dict(optim_state_dict)
+        ...
+    ```
+    Args:
+        state_dict (Dict[str, Any]) : A state_dict to load to. Note that this
+            state dict will updated in places.
+        storage_reader (StorageReader): An instance of storage loader.
+    """
+
+    metadata = storage_reader.read_metadata()
+    bytes_read_requests, tensor_read_requests = _reshard_and_prepare_read_request(
+        state_dict=state_dict, metadata_from_storage=metadata
+    )
+    bytes_futures = storage_reader.read_bytes(bytes_read_requests)
+    tensor_futures = storage_reader.read_tensors(tensor_read_requests)
+    bytes_futures.wait()
+
+    # Addtional steps are required to convert the bytes to its original type
+    # Note that this is NOT inplace,
+    # it creating a new object and replace what's in the state dict
+    for req in bytes_read_requests:
+        fqn = req.storage_key
+        # Ensure the BytesIO is rewound
+        req.bytes.seek(0)
+        state_dict[fqn] = torch.load(req.bytes)
+
+    tensor_futures.wait()
+
+
+def _validate_sharded_tensor(
+    tensor_md: ShardedTensorMetadata, checkpoint_md: ExtendedTensorMetadata
+) -> List[str]:
+    # We assume the incoming tensor has being validated during construction
+
+    res = []
+    # To ensure a checkpoint can satisfy loading a ST, we compute the loading
+    # plans for all shards and see if they are doable.
+    try:
+        # this API returns a list of issues
+        validate_non_overlapping_shards_metadata(
+            checkpoint_md.tensor_metadata.shards_metadata
+        )
+    except ValueError as e:
+        res.append(str(e))
+
+    for shard_md in tensor_md.shards_metadata:
+        read_volume = 0
+        for storage_md in checkpoint_md.storage_metadata:
+            shard_md_from_storage = storage_md.shard_metadata
+            assert shard_md_from_storage is not None
+            # this is a naive quadratic algo that can later be optimized by
+            #   sorting metadata and the shards md
+            # FIXME what does it mean for offset > 0?
+            assert (
+                storage_md.offset == 0
+            ), f"Cannot handle shard '{shard_md}': offset is non-zero"
+
+            # do they overlap?
+            if not _check_shard_metadata_pair_overlap(shard_md, shard_md_from_storage):
+                continue
+
+            shard_volume = 1
+            for (_, _, _, length,) in _shards_get_overlap_region_wrt_saved_tensor(
+                saved_shard=shard_md_from_storage, current_shard=shard_md
+            ):
+                shard_volume *= length
+            read_volume += shard_volume
+
+        shard_volume = 1
+        for size in shard_md.shard_sizes:
+            shard_volume *= size
+        if read_volume != shard_volume:
+            res.append(
+                f"Shard {shard_md} only has {read_volume} available" +
+                "elements but needs {shard_volume}"
+            )
+    return res
+
+
+def validate_metadata(
+    state_dict: Dict[str, Any], metadata: Metadata
+) -> Optional[List[str]]:
+    """
+    Verify if it's possible to correctly load `state_dict` from `metadata`.
+
+    This method can be used to validate if a checkpoint is usable with a given model.
+
+    Sample Code
+    ```
+        my_model: torch.nn.Model = ....
+        my_reader: torch.distributed._checkpoint.StorageReader = ...
+
+        res = torch.distributed._checkpoint.validate_metadata(my_model.state_dict(), my_reader.read_metadata())
+    ```
+    Args:
+        state_dict: A state_dict to verify if it's loadable.
+        metadata: Checkpoint metadata to verify against.
+
+    Returns:
+        None if no issue was found or a List[str] of issues.
+    """
+    res = []
+    for fqn, obj in state_dict.items():
+        if isinstance(obj, torch.Tensor):
+            if fqn not in metadata.state_dict_metadata:
+                res.append(f"{fqn}: Could not find Tensor metadata")
+                continue
+            md = metadata.state_dict_metadata[fqn]
+            md_size = list(md.tensor_metadata.size)
+            tensor_size = list(obj.size())
+            if md_size != tensor_size:
+                res.append(
+                    f"{fqn}: Incompatible tensor size: expected {tensor_size} but found {md_size}"
+                )
+        elif isinstance(obj, ShardedTensor):
+            if fqn not in metadata.state_dict_metadata:
+                res.append(f"{fqn}: Could not find ShardedTensor metadata")
+                continue
+            md = metadata.state_dict_metadata[fqn]
+            # Check if the overall ShardedTensor size is the same. Individual shards don't matter as we can reshard.
+            md_size = list(md.tensor_metadata.size)
+            tensor_size = list(obj.metadata().size)
+            if md_size != tensor_size:
+                res.append(
+                    f"{fqn}: Incompatible ShardedTensor size: expectected {tensor_size} but found {md_size}"
+                )
+            res += _validate_sharded_tensor(obj.metadata(), md)
+
+    return res if len(res) > 0 else None
---
+++ torch/distributed/_checkpoint/state_dict_saver.py
@@ -0,0 +1,189 @@
+import io
+from typing import Any, Dict, List, Tuple
+
+import torch
+import torch.distributed as dist
+from torch import Tensor
+from torch.distributed._shard.sharded_tensor import (
+    ShardedTensor,
+    ShardedTensorMetadata,
+    TensorProperties,
+    ShardMetadata,
+)
+
+from .metadata import (
+    Metadata,
+    BytesWriteRequest,
+    ExtendedTensorMetadata,
+    StorageMetadata,
+    TensorWriteRequest,
+)
+from .resharding import prepare_sharded_tensor_write
+from .storage_writer import StorageWriter
+
+# -------------- private functions --------------
+def _compute_tensor_md(fqn: str, tensor: Tensor) -> ExtendedTensorMetadata:
+    # --- Step 3, populate the metadata ---
+    #
+    # Since torch.Tensor does not have a standard set of metadata we can operate on
+    # We wrap troch.Tensor's metadata with ShardMetadata
+    # This is frankly a bad idea, I will need to change this
+    tensor = tensor.detach()
+    tensor_size = list(tensor.size())
+    storage_size = tensor.nelement() * tensor.element_size()
+    shard_metadata = ShardMetadata(
+        shard_offsets=[0] * len(tensor_size),
+        shard_sizes=tensor_size,
+        # Not sure how to deal with placement for regular tensor yet.
+        # Since they are only keep the copy on rank0, let's hard code it for now.
+        placement=f"rank:0/{str(tensor.device)}",
+    )
+
+    stm = ShardedTensorMetadata(
+        shards_metadata=[shard_metadata],
+        size=tensor.size(),
+        tensor_properties=TensorProperties(
+            dtype=tensor.dtype,
+            layout=tensor.layout,
+            requires_grad=tensor.requires_grad,
+            memory_format=torch.contiguous_format,
+            pin_memory=tensor.is_pinned(),
+        ),
+    )
+
+    return ExtendedTensorMetadata(
+        tensor_metadata=stm,
+        storage_metadata=[
+            StorageMetadata(
+                shard_metadata=shard_metadata,
+                storage_key=fqn,
+                length=storage_size,
+                offset=0,
+            )
+        ],
+    )
+
+
+def _prepare(
+    state_dict: Dict[str, Any], always_add_tensors: bool = False
+) -> Tuple[Metadata, Dict[str, int], List[BytesWriteRequest], List[TensorWriteRequest]]:
+    """
+    Uses the state_dict to build three things.
+
+    metadata: Metadata
+        The metatdata discribing the tensor / sharded tensor.
+        And it is storage meta data. See "../metadata.py" for detail
+
+    size_for_storage_keys: Dict[str, int]
+        Key is the storage key name, value is its size
+        It can used to pre allocate the storage for parallel and non sequential writes.
+
+    tensor_write_requests: List[TensorWriteRequest]
+        List of tensor write requests that should br perfromed by the writer.
+
+    bytes_write_requests: List[BytesWriteRequest]
+        List of byte write requests that should br perfromed by the writer.
+
+    Subclasses can optionally overwrite the implementation here,
+    if the default does not meet its requirement.
+    Args:
+        state_dict: The state_dict to operate on
+        always_add_tensors: Include non-sharded tensors even if rank != 0
+    """
+    metadata = Metadata(state_dict_metadata={})
+    tensor_write_requests: List[TensorWriteRequest] = []
+    bytes_write_requests: List[BytesWriteRequest] = []
+
+    for fqn, obj in state_dict.items():
+        if isinstance(obj, Tensor):
+            # The assumption is that non ShardedTensors are full replicated across all ranks
+            # So we just need one from Rank 0.
+            # If that's not the case, we will update later.
+            if (
+                not always_add_tensors
+                and dist.is_initialized()
+                and dist.get_rank() != 0
+            ):
+                pass
+            else:
+                tensor_write_requests.append(
+                    TensorWriteRequest(
+                        tensor=obj.detach(),
+                        storage_key=fqn,
+                    )
+                )
+                metadata.state_dict_metadata[fqn] = _compute_tensor_md(fqn, obj)
+        elif isinstance(obj, ShardedTensor):
+            write_reqs, md = prepare_sharded_tensor_write(obj, fqn)
+            tensor_write_requests += write_reqs
+            metadata.state_dict_metadata[fqn] = md
+        else:
+            bytes_io = io.BytesIO()
+            torch.save(obj, bytes_io)
+            bwr = BytesWriteRequest(
+                bytes=bytes_io,
+                storage_key=fqn,
+            )
+            bytes_write_requests.append(bwr)
+
+    storage_keys: Dict[str, int] = {
+        req.storage_key: req.tensor.nelement() * req.tensor.element_size()
+        for req in tensor_write_requests
+    }
+
+    return (metadata, storage_keys, bytes_write_requests, tensor_write_requests)
+
+
+def save_state_dict(
+    state_dict: Dict[str, Any],
+    storage_writer: StorageWriter,
+) -> None:
+    """
+    This public function defined the default behavior to save a state_dict
+    Notes
+    1. This is a WIP, the state_dict save with different versions of the code might not be compatible.
+    2. The caller needs to ensure the correctness of the state_dict
+
+    Sample Code
+    ```
+        my_model = MyModule()
+        optimizer = Adagrad(my_model.parameters())
+        ...
+
+        model_state_dict = my_model.state_dict()
+        optim_state_dict = optimizer.state_dict()
+
+        ...
+        # torch.distributed does not assume the the correctness of the state_dict
+        # the caller needs to ensure the correctness of the state_dict
+        optim_state_dict = some_function_to_cleanup_optim_state_dict(optim_state_dict)
+        ...
+
+        fs_storage_writer = torch.distributed._checkpoint.FileSystemWriter("/checkpoint/1")
+        torch.distributed._checkpoint.save_state_dict(
+            state_dict=model_state_dict,
+            storage_writer=fs_stroage_writer,
+        )
+        torch.distributed._checkpoint.save_state_dict(
+            state_dict=optim_state_dict,
+            storage_writer=fs_stroage_writer,
+        )
+        ...
+    ```
+
+    Args:
+        state_dict (Dict[str, Any]) : A state_dict
+        storage_writer (StorageWriter): An instance of storage writer that performance the writes.
+    """
+    (
+        metadata,
+        storage_keys,
+        bytes_write_requests,
+        tensor_write_requests,
+    ) = _prepare(state_dict)
+    storage_writer.prepare_storage(storage_keys=storage_keys)
+    storage_writer.write_metadata(metadata=metadata)
+    bytes_futures = storage_writer.write_bytes(bytes_write_requests)
+    tensor_futures = storage_writer.write_tensors(tensor_write_requests)
+    bytes_futures.wait()
+    tensor_futures.wait()
