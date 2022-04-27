51b09d4f9d77955e862810b359554e06fa068611
Rodrigo Kumpera <kumpera@fb.com>
2022-04-27 19:30:15 +0000

    Update the docs for storage interface.

--- torch/distributed/_shard/checkpoint/storage.py
+++ torch/distributed/_shard/checkpoint/storage.py
@@ -13,77 +13,155 @@ from .metadata import (
 
 class StorageWriter(abc.ABC):
     """
-    Interface to write to underlying storage system
+    Interface used by ``save_state_dict`` to write to storage.
+
+    A subclass should expect the following sequence of calls by ``save_state_dict``
+
+    1) prepare_storage with a dict of storage_key -> size. This can be used to
+        pre-allocate storage.
+    2) write_bytes
+    3) write_tensors.
+    4) (if rank == 0) write_metadata().
+
+    Rank 0 will wait for (2) and (3) before writing metadata.
+    The writes from (2) and (3) are initiated before any waiting is done.
     """
 
     @abc.abstractmethod
     def write_bytes(self, requests: List[BytesWriteRequest]) -> Future[None]:
         """
-        Performs a write request and returns a Future to wait on.
+        Initiate writes for all requests in `requests`.
+
+        Writing can happen asynchronously and/or concurrently. A blocking
+        implementation is valid.
+
         Args:
-            requests (BytesWriteRequest): see `./metadata.py`
+            requests (List[BytesWriteRequest]): A list of requests to write
+        Returns:
+            A future that completes once all writes have finished.
         """
         pass
 
     @abc.abstractmethod
     def write_tensors(self, requests: List[TensorWriteRequest]) -> Future[None]:
         """
-        Performs a write request and returns a Future to wait on.
+        Initiate writes for all requests in `requests`.
+
+        Writing can happen asynchronously and/or concurrently. A blocking
+        implementation is valid.
+
+        Implementors are responsible for any device to host transfers required
+        to copy.
+
         Args:
-            requests (TensorWriteRequest): see `./metadata.py`
+            requests (List[TensorWriteRequest]): A list of requests to write
+
+        Returns:
+            A future that completes once all writes have finished.
         """
         pass
 
     @abc.abstractmethod
     def write_metadata(self, metadata: Metadata) -> None:
         """
-        Writes the metatdata.
+        Writes the metadata.
+
+        This method is only called on rank 0.
+        This is called at the end of a checkpoint and signifies its completion.
 
         Args:
-            metadata (Metadata): see `./metadata.py`
+            metadata (Metadata): metadata for the new checkpoint
+
+        Returns:
+            None
         """
         pass
 
     def prepare_storage(self, storage_keys: Dict[str, int]) -> None:
         """
-        This blocking call can be overwritten by the subclass.
-        It can use `storage_keys` to plan for any write preformace optimization.
-        e.g. non sequential and parallel writes.
-        By default, it does nothing
+        Prepare the underlying storage for upcoming writes.
+
+        This is an optional override intended for advanced scenarios where
+        a storage layer needs wants to do some work ahead of the writing itself.
+
+        The default implementation does nothing.
+
+        NB: Size is a lowerbound estimate and doesn't account for any
+        serialization overhead from the storage layer. For example, for a tensor `t`
+        it will be `t.numel() * t.element_size()` but if `torch.save` is used,
+        there will be aditional serialization overhead.
 
         Args:
             storage_keys (Dict[str, int]): key - handle's name. value - size
                 of the handle.
+
+        Returns:
+            None
         """
         pass
 
 
 class StorageReader(abc.ABC):
     """
-    Interface to read from the underlying storage system.
+    Interface used by ``load_state_dict`` to read from storage.
+
+    A subclass should expected the following sequence of calls by ``load_state_dict``:
+
+    1) read_metadata() - on all ranks
+    2) read_bytes
+    3) read_tensors
+
+    The reads from (2) and (3) are initiated before any waiting is done.
+
+    Implementors must ensure host/device synchronization as part of
+    completion of both read requests.
     """
 
     @abc.abstractmethod
     def read_bytes(self, requests: List[BytesReadRequest]) -> Future[None]:
         """
-        Read request and returns a Future to wait on.
+        Initiate read for all requests in `requests`.
+
+        Reading happen asynchronously and/or concurrently. A blocking
+        implementation is valid.
+
         Args:
-            requests (List[BytesReadRequest]): see `./metadata.py`]
+            requests (List[BytesReadRequest]): A list of requests to read.
+
+        Return:
+            A future that completes once all read have finished.
         """
         pass
 
     @abc.abstractmethod
     def read_tensors(self, requests: List[TensorReadRequest]) -> Future[None]:
         """
-        Performs a read request and returns a Future to wait on.
+        Initiate read for all requests in `requests`.
+
+        Reading happen asynchronously and/or concurrently. A blocking
+        implementation is valid.
+
+        Implementors must not assume that the original device
+        at write time will be the same at read time.
+
+        If an implementation uses asynchronous copies to device, it must
+        ensure proper synchronization W.R.T. the returned future.
+
         Args:
-            requests (List[BytesReadRequest]): see `./metadata.py`
+            requests (List[BytesReadRequest]): A list of requests to read.
+
+        Returns:
+            A future that completes once all read have finished.
         """
         pass
 
     @abc.abstractmethod
     def read_metadata(self) -> Metadata:
         """
-        Read the meta data and returns.
+        Reads the checkpoint metadata.
+
+        Returnss:
+            The metatada object associated with the checkpoint being loaded.
+
         """
         pass
